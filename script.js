490dfba54cd034ce9ca2a0af3eafda0c09bae924
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-08-06 14:27:25 -0500

    finish up the cleaning portion of the assignment

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
 // Assignment Code
-var passLengthEl = prompt("How many character long password would you like?", "");
-var upperCharEl = confirm("Do you want upper case letters in the password?");
-var lowerCharEl = confirm("Do you want lower case letters in the password?");
-var numberCharEl = confirm("Do you want numbers in the password?");
-var specialCharEl = confirm("Do you want special characters in the password?");
+//Setting up global variables to use throughout the code
+var passLengthEl;
+var upperCharEl;
+var lowerCharEl;
+var numberCharEl;
+var specialCharEl;
+var passwordLength;
 var generateBtn = document.querySelector("#generate");
 
+//Setting up the arrays for different types of criteria for password characters
 var upperCaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var numberChar = ['0','1','2','3','4','5','6','7','8','9'];
-var specialChar = ['~',
-                    '`',
-                    '!',
-                    '@',
-                    '#',
-                    '$',
-                    '%',
-                    '^',
-                    '&',
-                    '*',
-                    '(',
-                    ')',  
-                    '-',
-                    '_',
-                    '+',
-                    '=',
-                    '[',
-                    ']',
-                    '{',
-                    '}',
-                    '|',
-                    ';',
-                    ',',
-                    '/',
-                    ':',
-                    '<',
-                    '>',
-                    '?'];
+var specialChar = ['~','`','!','@','#','$','%','^','&','*','(',')','-','_','+','=','[',']','{','}','|',';',',','/',':','<','>','?'];
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -44,12 +24,15 @@ function writePassword() {
   passwordText.value = password;
 }
 
+//generating the password based on the user inputs
 function generatePassword(){
-  var passwordLength = parseInt(passLengthEl);
+
+  //calls the password length function 
+  getPasswordLength();
+
   var passwordMerge = [];
-  if (passwordLength < 7 || passwordLength > 128){
-    return alert("Please pick a password length between 8 and 128.");
-  }else {
+  
+  //Checks the user criterias and merges the different type of array into one. 
     if(upperCharEl){
       passwordMerge = passwordMerge.concat(upperCaseLetters);
     }
@@ -62,13 +45,49 @@ function generatePassword(){
     if(specialCharEl){
       passwordMerge = passwordMerge.concat(specialChar);
     }
+
+    //generating the random password based on the password length required. 
     var passwordRandom = [];
     for (var i = 0; i < passwordLength; i++){
       var randomNum = Math.floor(Math.random() * passwordMerge.length);
       passwordRandom.push(passwordMerge[randomNum]); 
     }
-    return passwordRandom.join('');
+
+    return passwordRandom.join('');//returns the random password
+}
+
+//Asking the user for the password length
+function getPasswordLength(){
+  passLengthEl = prompt("How many character long password would you like?", "");
+  passwordLength = parseInt(passLengthEl);
+  checkPasswordLength(passwordLength); //Checking if the password is between the required characters. 
+}
+
+//Checking if the password is between the required characters.
+function checkPasswordLength(passwrd_Length){
+
+  if (passwrd_Length < 7 || passwrd_Length > 128){
+    alert("Please pick a password length between 8 and 128 characters.");
+    getPasswordLength();
+  }
+  else {
+    getCriteria();
   }
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+//Asking the user for the password criterias
+function getCriteria(){
+  upperCharEl = confirm("Do you want upper case letters in the password?");
+  lowerCharEl = confirm("Do you want lower case letters in the password?");
+  numberCharEl = confirm("Do you want numbers in the password?");
+  specialCharEl = confirm("Do you want special characters in the password?");
+  checkCriteria(); //Checking if the atleast one password criteria is sent. 
+}
+
+//Checking if the atleast one password criteria is sent.
+function checkCriteria(){
+    if(upperCharEl === false && lowerCharEl  === false && numberCharEl  === false && specialCharEl === false ){
+      alert("Please pick atleast one password criteria.");
+      getCriteria();
+    }
+}
